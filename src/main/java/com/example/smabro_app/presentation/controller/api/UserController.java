8705e80237491dc73a494945b9cc355359a6d30a
DuckyHacker <yuhi.baske0222@i.softbank.jp>
2021-03-25 19:34:31 +0900

    確認

--- src/main/java/com/example/smabro_app/presentation/controller/api/UserController.java
+++ src/main/java/com/example/smabro_app/presentation/controller/api/UserController.java
@@ -4,6 +4,7 @@ import com.example.smabro_app.application.facade.UserFacade;
 import com.example.smabro_app.presentation.dto.request.UserRequest;
 import com.example.smabro_app.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
